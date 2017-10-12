8f5416262b67a7bbaf03c5261b8972ace4dd4efe
Jeff Abrahamson <jeff@purple.com>
2017-10-12 16:33:52 +0200

    Activate python environment

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@
 # p27-src all in the same directory so that ../ gets me on my way to
 # the others.
 
+. venv/bin/activate
 ../p27-gen/p27gen.py --src ../p27-src/site-src --dst site/    \
 		     --config ../p27-src/site-config/         \
 		     $*
