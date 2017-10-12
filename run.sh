badc38ad1a26870ea7c861d1980bc974b614ca97
Jeff Abrahamson <jeff@purple.com>
2017-10-12 13:42:48 +0200

    First draft of p27, based on purple.com

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Additional arguments are meant for flags like verbose and dryrun.
+# For anything more complicated, just run purple.py by hand.
+#
+# This will work when run on my boxes where I have p27, p27-gen, and
+# p27-src all in the same directory so that ../ gets me on my way to
+# the others.
+
+../p27-gen/p27gen.py --src ../p27-src/site-src --dst site/    \
+		     --config ../p27-src/site-config/         \
+		     $*
